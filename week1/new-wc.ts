e560c9f21287af6471575a0bc20a642fc2bcef82
mbaula <markbaula.1@gmail.com>
2024-09-19 13:43:44 -0400

    step 5

--- week1/new-wc.ts
+++ week1/new-wc.ts
@@ -2,30 +2,36 @@ import * as fs from 'fs';
 
 const optionsMap: { [key: string]: (filePath: string) => number } = {
     '-c': (filePath) => fs.statSync(filePath).size, 
-    '-l': (filePath) => fs.readFileSync(filePath, 'utf-8').split('\n').length, 
+    '-l': (filePath) => fs.readFileSync(filePath, 'utf-8').split('\n').length-1, 
     '-w': (filePath) => fs.readFileSync(filePath, 'utf-8').split(/\s+/).filter(word => word.length > 0).length, 
     '-m': (filePath) => fs.readFileSync(filePath, 'utf-8').length 
 };
 
 function handleArguments(args: string[]): void {
-    if (args.length !== 2) {
+    let filePath: string;
+    let options: string[];
+
+    if (args.length === 1) {
+        filePath = args[0];
+        options = ['-l', '-w', '-c'];
+    } else if (args.length === 2) {
+        filePath = args[1];
+        options = [args[0]];
+    } else {
         console.log('Usage: new-wc [-c | -l | -w | -m] <file-path>');
         return;
     }
 
-    const option = args[0];
-    const filePath = args[1];
-
-    const countFunction = optionsMap[option];
-
-    if (!countFunction) {
-        console.log('Invalid option. Usage: new-wc [-c | -l | -w | -m] <file-path>');
-        return;
-    }
-
     try {
-        const result = countFunction(filePath);
-        console.log(`${result} ${filePath}`);
+        const results = options.map(option => {
+            const countFunction = optionsMap[option];
+            if (!countFunction) {
+                throw new Error('Invalid option');
+            }
+            return countFunction(filePath);
+        });
+
+        console.log(`${results.join(' ')} ${filePath}`);
     } catch (err) {
         if (err instanceof Error) {
             console.error(`Error: ${err.message}`);
